24841244b0bbb11365ccee0c8c7f9e544590bfae
kucaahbe <kucaahbe@ukr.net>
2011-01-08 05:03:23 +0200

    more rspec-style writing

--- lib/rspec/rails/views/matchers/have_tag.rb
+++ lib/rspec/rails/views/matchers/have_tag.rb
@@ -1,16 +1,22 @@
-RSpec::Matchers.define :have_tag do |tag,*options|
-  require 'nokogiri'
+require 'nokogiri'
 
-  match do |document|
-    options = options.shift
-    parsed_html = Nokogiri::HTML(document)
-    node_set = parsed_html.css(tag)
+module RSpec
+  module Matchers
+    def have_tag(tag,options={},&block)
+      Matcher.new :have_tag, tag, options, block do |_tag_, _options_, _block_|
 
-    result = [!node_set.first.nil?]
-    if options
-      result << (node_set.count == options[:count]) if options.has_key?(:count)
-      result << node_set.any? {|node| node.content =~ Regexp.new(options[:text]) } if options.has_key?(:text)
+	match do |document|
+	  parsed_html = Nokogiri::HTML(document)
+	  node_set = parsed_html.css(tag)
+
+	  result = [!node_set.first.nil?]
+	  if options
+	    result << (node_set.count == options[:count]) if options.has_key?(:count)
+	    result << node_set.any? {|node| node.content =~ Regexp.new(options[:text]) } if options.has_key?(:text)
+	  end
+	  result.all? {|boolean| boolean==true }
+	end
+      end
     end
-    result.all? {|boolean| boolean==true }
   end
 end
