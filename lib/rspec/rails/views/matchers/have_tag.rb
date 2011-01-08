926bffc556e9f8d88506f204187c007ad755e899
kucaahbe <kucaahbe@ukr.net>
2011-01-08 03:42:43 +0200

    added options

--- lib/rspec/rails/views/matchers/have_tag.rb
+++ lib/rspec/rails/views/matchers/have_tag.rb
@@ -1,7 +1,16 @@
-RSpec::Matchers.define :have_tag do |selector|
+RSpec::Matchers.define :have_tag do |tag,*options|
   require 'nokogiri'
+
   match do |document|
+    options = options.shift
     parsed_html = Nokogiri::HTML(document)
-    !parsed_html.css(selector).first.nil?
+    node_set = parsed_html.css(tag)
+
+    result = [!node_set.first.nil?]
+    if options
+      result << (node_set.count == options[:count]) if options.has_key?(:count)
+      result << node_set.any? {|node| node.content =~ Regexp.new(options[:text]) } if options.has_key?(:text)
+    end
+    result.all? {|boolean| boolean==true }
   end
 end
--- spec/matchers/have_tag_spec.rb
+++ spec/matchers/have_tag_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
 describe 'have_tag' do
-  it "should find <div>" do
-    rendered.should have_tag('div')
+
+  context "simple matching:" do
+
+    it "should find <div>" do
+      rendered.should have_tag('div')
+    end
+
+    it "should not find <strong>" do
+      rendered.should_not have_tag('strong')
+    end
+
+    it "should find 3 tags <p>" do
+      rendered.should have_tag('p',:count => 3)
+    end
+
+    it "should not find 2 or 5 tags <p>" do
+      rendered.should_not have_tag('p', :count => 2)
+      rendered.should_not have_tag('p', :count => 5)
+    end
+
+    it "should find text" do
+      rendered.should have_tag('div', :text => 'sample text')
+      rendered.should have_tag('p', :text => 'one')
+      rendered.should have_tag('p', :text => 'two')
+      rendered.should have_tag('p', :text => 'three')
+    end
+
   end
 
-  it "should not find <strong>" do
-    rendered.should_not have_tag('strong')
-  end
 end
