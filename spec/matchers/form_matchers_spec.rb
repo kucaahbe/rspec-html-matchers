000e8fef0f8bfaf8e919e12f31bc56c9a1926d91
kucaahbe <kucaahbe@ukr.net>
2011-03-03 08:12:44 +0200

    added with_select

--- spec/matchers/form_matchers_spec.rb
+++ spec/matchers/form_matchers_spec.rb
@@ -5,6 +5,26 @@ describe "have_form" do
   before :each do
     render_html <<HTML
 <form action="/books" class="formtastic book" id="new_book" method="post">
+
+  <div style="margin:0;padding:0;display:inline">
+    <input name="authenticity_token" type="hidden" value="718WaH76RAbIVhDlnOidgew62ikn8IUCOyWLEqjw1GE=" />
+   </div>
+
+  <fieldset class="inputs">
+    <ol>
+      <li class="select required" id="book_publisher_input">
+        <label for="book_publisher_id">
+           Publisher<abbr title="required">*</abbr>
+        </label>
+	<select id="book_publisher_id" name="book[publisher_id]">
+	  <option value=""></option>
+	  <option value="1">The Pragmatic Bookshelf</option>
+	  <option value="2">sitepoint</option>
+	  <option value="3">O'Reilly</option>
+	</select>
+      </li>
+    </ol>
+  </fieldset>
 </form>
 HTML
   end
@@ -27,7 +47,27 @@ HTML
 
   context "with &block" do
 
-    it "TODO"
+    context "with_select" do
+
+      it "should find select" do
+	rendered.should have_form("/books", :post) do
+	  with_select("book[publisher_id]", :with => { :id => "book_publisher_id" })
+
+	  self.should_receive(:have_tag).with("select#book_publisher_id", :with => { :name => "book[publisher_id]" })
+	  with_select("book[publisher_id]", :with => { :id => "book_publisher_id" })
+	end
+      end
+
+      it "should not find select" do
+	rendered.should have_form("/books", :post) do
+	  without_select("book[publisher_id]", :with => { :id => "other_id" })
+
+	  self.should_receive(:have_tag).with("select#book_publisher_id", :with => { :name => "koob[publisher_id]" })
+	  without_select("koob[publisher_id]", :with => { :id => "book_publisher_id" })
+	end
+      end
+
+    end
 
   end
 end
