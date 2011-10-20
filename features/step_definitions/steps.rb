be776e16b44cfea04e703c95c49678249f61b037
kucaahbe <kucaahbe@ukr.net>
2011-10-21 00:39:56 +0300

    added gem's matchers

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -9,9 +9,19 @@ When /^I open this template in browser$/ do
 end
 
 Then /^I should be able to match static content$/ do
+  # capybara:
   page.should have_content('Hello World!')
+  page.should have_css('h1')
+
+  # rspec2 matchers:
+  page.should have_tag('h1', :text => 'Hello World!')
 end
 
 Then /^I should be able to match javascript generated content$/ do
+  # capybara:
   page.should have_content('Hello Another World!')
+  page.should have_css('p')
+
+  # rspec2 matchers:
+  page.should have_tag('p', :text => 'Hello Another World!')
 end
