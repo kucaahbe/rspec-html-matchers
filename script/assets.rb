2d99eba5faa1e35326bb8852b205fcbeb7c80741
Dmitry Mjakotny <kucaahbe@ukr.net>
2011-11-06 15:35:51 +0200

    script folder with dev tools

---
+++ script/assets.rb
@@ -0,0 +1,37 @@
+class Asset
+  @@assets = {}
+
+  PATH = File.expand_path(File.join(File.dirname(__FILE__),'..','assets','*.html'))
+
+  def self.[] name
+    @@assets[name.to_s]
+  end
+
+  def self.<< asset
+    @@assets.merge! asset.name => asset
+  end
+
+  def self.list
+    @@assets.keys
+  end
+
+  attr_reader :name
+
+  def initialize name, path
+    @name = name
+    @path = path
+  end
+
+  def content
+    @content ||= IO.read(@path)
+    puts @content
+  end
+  alias_method :c, :content
+end
+
+puts 'assets list:'
+Dir[Asset::PATH].each do |asset_path|
+  asset_name = File.basename(asset_path,'.html')
+  puts asset_name
+  Asset << Asset.new(asset_name,asset_path)
+end
