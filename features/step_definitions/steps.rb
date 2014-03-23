75520411253dba160a1453cc9e5bf83de2e639fe
kucaahbe <kucaahbe@ukr.net>
2014-03-23 18:20:50 +0200

    wow, new expect syntax everywhere

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -10,18 +10,18 @@ end
 
 Then /^I should be able to match static content$/ do
   # capybara:
-  page.should have_content('Hello World!')
-  page.should have_css('h1')
+  expect(page).to have_content('Hello World!')
+  expect(page).to have_css('h1')
 
-  # rspec2 matchers:
-  page.should have_tag('h1', :text => 'Hello World!')
+  # rspec-html-matchers:
+  expect(page).to have_tag('h1', :text => 'Hello World!')
 end
 
 Then /^I should be able to match javascript generated content$/ do
   # capybara:
-  page.should have_content('Hello Another World!')
-  page.should have_css('p')
+  expect(page).to have_content('Hello Another World!')
+  expect(page).to have_css('p')
 
-  # rspec2 matchers:
-  page.should have_tag('p', :text => 'Hello Another World!')
+  # rspec-html-matchers:
+  expect(page).to have_tag('p', :text => 'Hello Another World!')
 end
