d7b0f18cec33001c88a641235c3739081e0557e9
Tiago Cardoso <tiago@restorm.com>
2014-05-24 13:55:38 +0200

    deprecated redundant setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,6 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-
   config.filter_run_excluding wip: true
 
   config.extend AssetHelpers
